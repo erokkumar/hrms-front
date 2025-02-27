2f42a31ac701e849c7e4512090b2a9c1b1cfff2e
ROHIT KUMAR <101036004+erokkumar@users.noreply.github.com>
2025-02-27 16:32:43 +0530

    Co-authored-by: Rohitshaw1999 <Rohitshaw1999@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./Authentication/Login";
+import Dashboard from "./Componets/deshboard/Dashboard";
+import Leves from "./Componets/leaves/Leves";
+import Task from "./Componets/task/Task";
+import Tickets from "./Componets/Tickets/Tickets";
+import PrivateRoute from "./PrivateRoute"; // Import the PrivateRoute component
+import Employee from "./Componets/Employee/Employee";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App aspect-ratio-content">
+        <Routes>
+          {/* Public Route */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected Routes */}
+          <Route path="/" element={<PrivateRoute element={<Dashboard />} />} />
+          <Route path="/leaves" element={<PrivateRoute element={<Leves />} />} />
+          <Route path="/tasks" element={<PrivateRoute element={<Task />} />} />
+          <Route path="/tickets" element={<PrivateRoute element={<Tickets />} />} />
+          <Route path="/employee" element={<PrivateRoute element={<Employee />} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
